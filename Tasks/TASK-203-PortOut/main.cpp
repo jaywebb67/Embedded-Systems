d4e96133d7050d2ee809b875fc8a3f5fe4b0ac2d
jaywebb67 <66364757+jaywebb67@users.noreply.github.com>
2022-10-06 13:40:53 +0100

    task203

--- Tasks/TASK-203-PortOut/main.cpp
+++ Tasks/TASK-203-PortOut/main.cpp
@@ -4,18 +4,23 @@
 #define TRAF_GRN1_PIN PC_6
 #define TRAF_YEL1_PIN PC_3
 #define TRAF_RED1_PIN PC_2
-#define LEDMASK 0b0000000001001100
+#define LEDMASK 0b0000000001001000
+#define LEDMASKboard 0b010000001001001
 // Objects
 //BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN);
 PortOut leds(PortC, LEDMASK);
-
+PortOut onBoardLeds(PortC, LEDMASKboard);
 int main()
 {
     while (true) {
         leds = 0;   //Binary 000
         wait_us(500000);
         leds = 0xFF;   //Binary 11111111
-        wait_us(500000);    
+        wait_us(500000); 
+        leds ^= LEDMASK;
+        wait_us(500000)
+        leds ^= LEDMASK;
+        wait_us(500000)
     }
 }
 

79bdcad3d279ec70f080ad7a65ab48b80a3e0814
jaywebb67 <66364757+jaywebb67@users.noreply.github.com>
2022-11-18 22:35:40 +0000

    360B

--- Tasks/Task-360B-ISR/Flashing.hpp
+++ Tasks/Task-360B-ISR/Flashing.hpp
@@ -10,33 +10,29 @@ class flashing {
     private:
         DigitalOut  _ledPin;
         Ticker yellowTick;
-        };
+
+
+    private:
+
+        void toggleYellowISR(){
+            _ledPin = !_ledPin;
+        }
 
     public:
 
         //Constructor turning on LED when object is created
         flashing(PinName ledPin) : _ledPin(ledPin) {
-
                 //Simple ticker for the yellow LED
-                _ledPin = 1;
                 yellowTick.attach(&flashing::toggleYellowISR,500ms);
         }
+
         //Destructor, ensuring led is switched off when out of scope
         ~flashing() {
           _ledPin = 0;
-          yellowTick.dettach();
+          yellowTick.detach();
         }
  
-
-        void toggleYellowISR(){
-
-            _ledPin = !_ledPin;
-        }
 };
-
-
-
-
 
 
 #endif
